640bdb516f1f82af18a992990d1f4983646db091
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-16 14:14:33 +0100

    Example of module be executed when required in another module, if it has been called in the original module. No need to export the module inorder to invoke it.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 const {john, peter} = require("./names.js");
 const {sayHi} = require("./utils.js");
 const data = require("./alternative.js");
+require("./mind-grenade.js");
 
-sayHi("Moolie");
-sayHi(john);
-sayHi(peter);
-
-console.log(john + " " + peter);
-console.log(data);
+// sayHi("Moolie");
+// sayHi(john);
+// sayHi(peter);
+//
+// console.log(john + " " + peter);
