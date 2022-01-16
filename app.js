6aee336ec92fb6f64d4e2d8c36dd611708c377ec
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-16 14:05:10 +0100

    Alternative syntax for same module exports.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 
 const {john, peter} = require("./names.js");
 const {sayHi} = require("./utils.js");
+const data = require("./alternative.js");
 
 sayHi("Moolie");
 sayHi(john);
 sayHi(peter);
 
-console.log(names.john + " " + names.peter);
+console.log(john + " " + peter);
+console.log(data);
