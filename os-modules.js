ee90868e11e5e1831d156c4b25c38cf533a298e3
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-17 21:52:19 +0100

    Go through some built in node modules. OS modules.

---
+++ os-modules.js
@@ -0,0 +1,20 @@
+// 1. Operating system os-modules
+
+const os = require("os");
+
+// info about current user
+const user = os.userInfo();
+console.log(user);
+
+// method returns the system uptime is seconds
+console.log(`The System Uptime is: ${os.uptime()} seconds`);
+
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem()
+}
+
+console.log(currentOS);
