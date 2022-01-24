c2269ba38fe580eea781d704d1aeda4969e4a15a
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-24 20:18:27 +0100

    Four examples of event loop.

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
-const _ = require("lodash");
+//  Event Loop Examples
 
-const items = [1, [2, [3, 4, [5, 6]]]];
+// Example one
+// const {readFile} = require("fs");
+//
+// console.log("Started a first task...");
+//
+// readFile("./content/first.txt", "utf8", (err, result)=> {
+//   if(err){
+//     return;
+//     console.log(err);
+//   }
+//   console.log(result);
+//   console.log("Complete the first task.");
+// });
+//
+// console.log("Starting another task.");
 
-// Use lodash flattenDeep method to turn items array into a flat array
-const newItems = _.flattenDeep(items);
 
-console.log(newItems);
-console.log("Testing, messing...");
+// Example two
+// Start Operating System process
+// console.log("First");
+//
+// setTimeout(()=> {
+//   console.log("Second");
+// } ,0);
+//
+// console.log("Third");
+// Completed and ended Operating process
+
+
+// Example three
+// setInterval(()=> {
+//   console.log("Hallo!");
+// },2000);
+// console.log("I'll be executed first.");
+
+// Every 2 seconds the event loop gonna envoke setInterval call back and the  process stays alive unless:
+// kill it with ctrl + c
+// unexpected error
+
+
+
+// Example four
+const http = require("http");
+const server = http.createServer((req, res)=> {
+  console.log("Request event");
+  res.end("Hello Everybody!");
+});
+
+server.listen(3000, ()=>{
+  console.log("Server is up and running...");
+});
---
+++ lodash.js
@@ -0,0 +1,9 @@
+const _ = require("lodash");
+
+const items = [1, [2, [3, 4, [5, 6]]]];
+
+// Use lodash flattenDeep method to turn items array into a flat array
+const newItems = _.flattenDeep(items);
+
+console.log(newItems);
+console.log("Testing, messing]...");
