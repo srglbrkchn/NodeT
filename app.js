81c8d02cb2274c3e2867ec71fbe400d225d5ca80
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-27 13:27:16 +0100

    Use util.promisify functionality to return promise with readFile and writeFile and simplify the code.

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 // Cleaner async code
 // Using promise instead on nested callback
 
-const {readFile} = require("fs");
+const {
+  readFile, writeFile
+} = require("fs");
 
-const getText = (path)=> {
-  return new Promise((resolve, reject)=> {
-    readFile(path, "utf8", (err, data)=> {
-      if(err) {
-        reject(err);
-      }else {
-        resolve(data);
-      }
-    });
-  });
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
+
+
+const start = async () => {
+  try {
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise("./content/result-promisify.txt", `This is easy... hahaha ${first} ${second}`);
+    console.log(first, second);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-getText("../content/first.txt").
-then((data)=> {console.log(data);}).
-catch((err)=> {console.log(err);});
+start();
+
+// const getText = (path) => {
+//   return new Promise((resolve, reject) => {
+//     readFile(path, "utf8", (err, data) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   });
+// }
+
+
+// getText("../content/first.txt").
+// then((data)=> {console.log(data);}).
+// catch((err)=> {console.log(err);});
