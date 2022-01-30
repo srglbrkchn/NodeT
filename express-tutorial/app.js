821c98205466aa5ce11de5a38ad5bd6930b74bcb
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-30 18:56:41 +0100

    Study status code and mime-type.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -2,5 +2,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
+ // Use .writeHead method to provide hedear/Meta data about our response
+ res.writeHead(200, {"content-type":"text/html"});
+ res.write("<h1> Home page </h1>");
+ res.end();
+});
 
+server.listen(3000, () => {
+  console.log("Server is up and running...");
 });
