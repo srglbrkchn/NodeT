55a33e1b9b99537d5a5c685dae226c0eb5711182
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-01-31 12:29:34 +0100

    Cover request objects, status code and response.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -2,10 +2,37 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
- // Use .writeHead method to provide hedear/Meta data about our response
- res.writeHead(200, {"content-type":"text/html"});
- res.write("<h1> Home page </h1>");
- res.end();
+  // Use .writeHead method to provide hedear/Meta data about our response
+  // Using mime/media type
+
+  const url = req.url;
+
+  // Home page
+  if (url === "/") {
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    res.write("<h1> Home page </h1>");
+    res.end();
+  }
+   // About page
+  else if (url === "/about") {
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    res.write("<h1> About page </h1>");
+    res.end();
+
+  }
+  // 404
+  else {
+    res.writeHead(404, {
+      "content-type": "text/html"
+    });
+    res.write("<h1> Page not found!!</h1>");
+    res.end();
+  }
+
 });
 
 server.listen(3000, () => {
