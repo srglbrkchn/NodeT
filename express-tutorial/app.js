faae779b0fadad8009c00822b7e513e32816d73b
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-02 09:27:10 +0100

    Express basic examples.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -1,79 +1,36 @@
-// Setting up the server
-const http = require("http");
-const {
-  readFileSync
-} = require("fs");
+// Setting up the server using Express
+const express = require("express");
 
-// Get all files
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyles = readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
+// app an instanse of express
+const app = express();
 
+// or
+// const app = require("express")();
 
-// set up the server
-const server = http.createServer((req, res) => {
-  // Use .writeHead method to provide hedear/Meta data about our response
-  // Using mime/media type
-
-  const url = req.url;
-  console.log(url);
-
-  // Home page
-  if (url === "/") {
-    res.writeHead(200, {
-      "content-type": "text/html"
-    });
-    res.write(homePage);
-    res.end();
-  }
-  // About page
-  else if (url === "/about") {
-    res.writeHead(200, {
-      "content-type": "text/html"
-    });
-    res.write("<h1> About page </h1>");
-    res.end();
-  }
-
-  // Styles
-  else if (url === "/styles.css") {
-    res.writeHead(200, {
-      "content-type": "text/css"
-    });
-    res.write(homeStyles);
-    res.end();
-  }
-
-  // Image - logo
-  else if (url === "/logo.svg") {
-    res.writeHead(200, {
-      "content-type": "image/svg+xml"
-    });
-    res.write(homeImage);
-    res.end();
-  }
-
-  // Logic
-  else if (url === "/browser-app.js") {
-    res.writeHead(200, {
-      "content-type": "text/javascript"
-    });
-    res.write(homeLogic);
-    res.end();
-  }
-
-  // 404
-  else {
-    res.writeHead(404, {
-      "content-type": "text/html"
-    });
-    res.write("<h1> Page not found!!</h1>");
-    res.end();
-  }
-
+app.get("/",(req, res)=> {
+  console.log("User is asking for the resource...");
+  res.status(200).send("Home page :) ");
 });
 
-server.listen(3000, () => {
-  console.log("Server is up and running...");
+app.get("/about", (req, res)=> {
+  res.status(200).send("About page");
 });
+
+// handles all http verbs/cases
+app.all("*", (req, res)=> {
+  res.status(404).send("<h1> Resource Not Found! </h1>");
+});
+
+
+app.listen(3000, ()=> {
+  console.log("The server is up and running...");
+});
+
+// app's methods
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
