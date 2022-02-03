15f59bc1a87942fda0a068419719da36bbe66561
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-03 18:04:25 +0100

    Use map function to send back selected portion of the data on user request.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -7,7 +7,16 @@ const {products} = require("./data.js");
 
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send("<h1>Home Page </h1> <a href='/api/products'>products</a>");
+});
+
+app.get("/api/products", (req, res)=> {
+  const newProducts = products.map((product) => {
+    const {id, name, image} = product;
+    return {id, name, image};
+  });
+
+  res.json(newProducts);
 });
 
 
