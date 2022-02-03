279b667a1698201931b04745299185fdb6fad6fa
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-03 14:03:02 +0100

    Finish with initial express set up. Serve the site without senFile method using only static assets.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -4,12 +4,16 @@ const app = express();
 const path = require("path");
 
 // Set up static and middleware using use verb
+// express.static => a built in middleware
+// put static assets (files that server does not need to change them in public folder)
 app.use(express.static("./public"));
 
-app.get("/", (req, res)=> {
-  // sending absolute path using resolve method in path module
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-});
+// app.get("/", (req, res)=> {
+//   // sending absolute path using resolve method in path module
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding to static assets
+// Server Side Rendering (SSR) => Using template engine for that
+// });
 
 
 // In case of 404
