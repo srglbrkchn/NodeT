54ed9e23a029b258192f747416778925a4c32b2c
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-08 13:43:48 +0100

    Use multiple middleware function.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -3,11 +3,17 @@
 const express = require("express");
 const app = express();
 const logger = require("./logger.js");
+const authorize = require("./authorize.js");
 
 // app.use applies the middleware to all the routes
 // if we specify a path here, it becomes the root of the path that invokes the middleware function
-// and the middleware function only be executed if the base of requested path matches the path
-app.use("/api", logger);
+// and the middleware function only be executed if the base of requested path matches the root here
+// app.use("/api", logger);
+
+// When path ommited, middleware function will be applied to all the requests
+
+// use multiple middleware functions, order is important, executed based on order
+app.use([logger ,authorize]);
 
 app.get("/", (req, res) => {
   res.send("Home");
