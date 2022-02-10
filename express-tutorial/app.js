e55269b46ba484352c38df916ba610650232689e
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-10 16:50:21 +0100

    Cover all http verbs.

--- express-tutorial/app.js
+++ express-tutorial/app.js
@@ -29,6 +29,59 @@ app.post("/api/people", (req, res) => {
   }
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const {name} = req.body;
+
+  if(!name) {
+    return(res.status(400).json({success:false, msg: "Please provide a name."}));
+  }else{
+    return(res.status(201).json({success:true, data: [...people, name]}));
+  }
+});
+
+// put method for update
+app.put("/api/people/:id", (req, res) => {
+  const {id} = req.params;
+  const {name} = req.body;
+  const person = people.find((person)=> {
+    if(person.id === Number(id)){
+      return person;
+    }
+  });
+
+  if(!person) {
+    res.status(404)
+    .json({success:false, msg:`No person with ID: ${id} exists.`});
+  }
+    const newPeople = people.map((person)=> {
+      if(person.id === Number(id)) {
+        person.name = name;
+        return person;
+      }else {
+        return person;
+      }
+    });
+  res.status(200).json({success:true, data: newPeople});
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const {id} = req.params;
+  const person = people.find((person)=> {
+    if(person.id === Number(id)){
+      return person;
+    }
+  });
+
+  if(!person) {
+    res.status(404).json({success:false, msg: `Person with ID ${id} does not exists`});
+  }
+
+  const newPeople = people.filter((person)=> {
+      return person.id !== Number(id);
+  });
+  res.status(200).json({success:true, data:newPeople});
+});
+
 
 app.post("/login", (req, res) => {
   const {name} = req.body;
