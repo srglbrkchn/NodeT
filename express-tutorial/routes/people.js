c3cf24e63ed8cbd320885773519b20f77187793a
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-14 11:32:59 +0100

    Chain router methods. Final commit.

--- express-tutorial/routes/people.js
+++ express-tutorial/routes/people.js
@@ -15,16 +15,15 @@ const {
 } = require("../controlers/people.js");
 
 
-router.get("/", getPeople);
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// // put method for update
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.post("/", createPerson);
-
-router.post("/postman", createPersonPostman);
-
-// put method for update
-router.put("/:id", updatePerson);
-
-router.delete("/:id", deletePerson);
-
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
