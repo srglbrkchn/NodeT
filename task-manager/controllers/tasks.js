c201a594b9d3d433e554a733f8a4e923d0c52b33
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-15 20:16:50 +0100

    Set up and test all needed routes and controlles + test them via postman.

---
+++ task-manager/controllers/tasks.js
@@ -0,0 +1,24 @@
+
+
+const getAllTasks = (req, res)=> {
+  res.send("All tasks from the file...");
+}
+
+const createTask = (req, res)=> {
+  res.json(req.body);
+}
+
+const getTask = (req, res) => {
+  res.json({id: req.params.id});
+}
+
+const updateTask = (req, res) => {
+  res.send("update a task");
+}
+
+const deleteTask = (req, res) => {
+  res.send("delete a task");
+}
+
+
+module.exports = {getAllTasks, createTask, getTask, updateTask, deleteTask};
---
+++ task-manager/routes/tasks.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const {getAllTasks, createTask, getTask, updateTask, deleteTask} = require("../controllers/tasks.js");
+
+// Setting up the express router
+const router = express.Router();
+
+
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+
+
+module.exports = router;
