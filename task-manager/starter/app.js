16d68557b37085388a72fe2fac89e1ed3c9d75a7
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2022-02-18 22:53:43 +0100

    Connect the application to mongodb Atlas. Create Schema for Tasks .

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-
 const port = 3000;
-
 const tasks = require("../routes/tasks.js");
+const connectDB = require("./db/connect.js");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -17,6 +17,17 @@ app.get("/hello", (req, res)=> {
 //  /api/v1/tasks => root route for the tasks router
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, ()=>{
-  console.log(`The server is up and running on port ${port}...`);
-});
+const start = async ()=> {
+  try {
+    // we await the promiss. if connection is successful we spin up the server
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, ()=>{
+      console.log(`The server is up and running on port ${port}...`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// envoke the start function
+start();
